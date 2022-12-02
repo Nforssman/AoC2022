94b4496e66b4d59b6c490da5499baea05fac2738
Niklas Forssman <niklas.forssman@hotmail.com>
2022-12-02 16:23:12 +0100

    day2

---
+++ day2/day2.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const argumentFile = process.argv[2];
+const DetermineScorePart1 = (round) => {
+  const [elf, me] = round.map((x) => x.split(",")).map(String);
+  if (elf === "A" && me === "X") return 1 + 3;
+  if (elf === "A" && me === "Y") return 2 + 6;
+  if (elf === "A" && me === "Z") return 3;
+  if (elf === "B" && me === "X") return 1;
+  if (elf === "B" && me === "Y") return 2 + 3;
+  if (elf === "B" && me === "Z") return 3 + 6;
+  if (elf === "C" && me === "X") return 1 + 6;
+  if (elf === "C" && me === "Y") return 2;
+  if (elf === "C" && me === "Z") return 3 + 3;
+};
+const DetermineScorePart2 = (round) => {
+  const [elf, me] = round.map((x) => x.split(",")).map(String);
+  if (elf === "A" && me === "X") return 3;
+  if (elf === "A" && me === "Y") return 1 + 3;
+  if (elf === "A" && me === "Z") return 2 + 6;
+  if (elf === "B" && me === "X") return 1;
+  if (elf === "B" && me === "Y") return 2 + 3;
+  if (elf === "B" && me === "Z") return 3 + 6;
+  if (elf === "C" && me === "X") return 2;
+  if (elf === "C" && me === "Y") return 3 + 3;
+  if (elf === "C" && me === "Z") return 1 + 6;
+};
+const parseInput = fs
+  .readFileSync(argumentFile, "utf-8")
+  .trim()
+  .split("\n")
+  .map((x) => x.split(" "));
+
+const part1 = () => {
+  return parseInput
+    .map((score) => DetermineScorePart1(score))
+    .reduce((prev, curr) => prev + curr, 0);
+};
+
+const part2 = () => {
+  return parseInput
+    .map((score) => DetermineScorePart2(score))
+    .reduce((prev, curr) => prev + curr, 0);
+};
+console.log(`Del 1: ${part1(parseInput)}`);
+console.log(`Del 22: ${part2(parseInput)}`);
