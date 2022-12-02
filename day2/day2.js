400fd4e5ff37bb3c053abbad70057dc956a0f533
Niklas Forssman <niklas.forssman@hotmail.com>
2022-12-02 16:26:41 +0100

    day2day2

--- day2/day2.js
+++ day2/day2.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const argumentFile = process.argv[2];
-const DetermineScorePart1 = (round) => {
+
+const DetermineRoundPart1 = (round) => {
   const [elf, me] = round.map((x) => x.split(",")).map(String);
   if (elf === "A" && me === "X") return 1 + 3;
   if (elf === "A" && me === "Y") return 2 + 6;
@@ -12,7 +13,8 @@ const DetermineScorePart1 = (round) => {
   if (elf === "C" && me === "Y") return 2;
   if (elf === "C" && me === "Z") return 3 + 3;
 };
-const DetermineScorePart2 = (round) => {
+
+const DetermineRoundPart2 = (round) => {
   const [elf, me] = round.map((x) => x.split(",")).map(String);
   if (elf === "A" && me === "X") return 3;
   if (elf === "A" && me === "Y") return 1 + 3;
@@ -24,6 +26,7 @@ const DetermineScorePart2 = (round) => {
   if (elf === "C" && me === "Y") return 3 + 3;
   if (elf === "C" && me === "Z") return 1 + 6;
 };
+
 const parseInput = fs
   .readFileSync(argumentFile, "utf-8")
   .trim()
@@ -32,14 +35,14 @@ const parseInput = fs
 
 const part1 = () => {
   return parseInput
-    .map((score) => DetermineScorePart1(score))
+    .map((round) => DetermineRoundPart1(round))
     .reduce((prev, curr) => prev + curr, 0);
 };
 
 const part2 = () => {
   return parseInput
-    .map((score) => DetermineScorePart2(score))
+    .map((round) => DetermineRoundPart2(round))
     .reduce((prev, curr) => prev + curr, 0);
 };
 console.log(`Del 1: ${part1(parseInput)}`);
-console.log(`Del 22: ${part2(parseInput)}`);
+console.log(`Del 2: ${part2(parseInput)}`);
